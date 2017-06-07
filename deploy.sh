f81a4da306a790680d6ec474398ce3d50002af17
Theoodore Rose <Theodore.O.Rose@gmail.com>
2017-06-06 22:41:20 -0400

    changed script data to me

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push willrstern/sample-node
+docker push trose/sample-node
 
-ssh deploy@159.203.127.59 << EOF
-docker pull willrstern/sample-node:latest
+ssh -i ~/.ssh/ocean_rsa deploy@build.me << EOF
+docker pull trose/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:latest willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker rmi trose/sample-node:current || true
+docker tag trose/sample-node:latest trose/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 trose/sample-node:current
 EOF
